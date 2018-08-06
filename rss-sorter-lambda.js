fcf08733946e48ce17c2b72b24e8bc95c90221b7
Jeff Woleslagle <jwoleslagle@gmail.com>
2018-08-06 09:20:49 -0400

    Array string working

--- rss-sorter-lambda.js
+++ rss-sorter-lambda.js
@@ -11,6 +11,7 @@ let parser = new Parser();
 const client = new Client();
 
 async function incomingFeed(feedURL) {
+  console.log('Parse incoming feed started.');
   try {
     let feedItems = await parser.parseURL(feedURL);
     console.log(feedItems);
@@ -19,9 +20,11 @@ async function incomingFeed(feedURL) {
   catch(e) {
     console.log(`Incoming feed error: ${e}`);
   }
+  console.log('Parse incoming feed finished.');
 }
 
 async function last20Items() {
+  console.log('Get last 20 guids started.');
   try {
     const guidArr = [];
     await client.connect();
@@ -35,29 +38,36 @@ async function last20Items() {
   catch(e) {
     console.log(`DB read error: ${e}`);
   }
+  console.log('Get last 20 guids finished.');
 }
 
 async function writeItemsToDb(newFeed, oldGuids) {
+  //TODO: Split this into two functions, one for filter, second for write.
+  console.log('Feed dupe filter started.');
   let itemsArr = [];
   newFeed.items.forEach((item) => {
     if (oldGuids.rows.includes(item.guid) === false) {
-      const itemToAdd = `('${item.title}', '${item.content}', '${item.link}', '${item.pubDate}', '${item.guid}')`;
+      const itemToAdd = ` ('${item.title}', '${item.content}', '${item.link}', '${item.pubDate}', '${item.guid}')`;
       itemsArr.push(itemToAdd);
     }
   })
+  console.log('Feed dupe filter finished.');
   console.log(itemsArr);
   let items = itemsArr.join();
+  items = items + ';';
   console.log(items);
-  // try {
-  //   const client2 = new Client();
-  //   await client2.connect();
-  //   await client2.query(`INSERT INTO "feedDetails" ("title", "description", "link", "pubDate", "guid", "feedID")
-  //   VALUES ${items};`);
-  //   await client2.end();
-  // }
-  // catch(e) {
-  //   console.log(`DB write error: ${e}`);
-  // }
+  console.log('Write feed to DB started.')
+  try {
+    const client2 = new Client();
+    await client2.connect();
+    await client2.query(`INSERT INTO "feedDetails" ("title", "description", "link", "pubDate", "guid", "feedID")
+    VALUES ${items};`);
+    await client2.end();
+  }
+  catch(e) {
+    console.log(`DB write error: ${e}`);
+  }
+  console.log('Write feed to DB finished.')
 }
 
 (async() => {
